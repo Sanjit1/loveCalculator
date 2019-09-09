39014383dd4648bd811cd1a750edb1a292cc615f
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-09-08 22:00:40 -0700

    Made Changes

---
+++ calculator.js
@@ -0,0 +1,34 @@
+var calculate = document.getElementById("calc");
+var person1 = document.getElementById("person1");
+var person2 = document.getElementById("crush");
+
+
+
+// Get a reference to the database service
+var database = firebase.database();
+
+calculate.onclick = async function () {
+    var chance = Math.floor(Math.random() * (+100 - +0)) + +0;
+    for (var i = 0; i < chance; i++) {
+        let delayres = await delay(40);
+        document.getElementById("p").innerHTML = i + "%";
+    }
+
+    firebase.database().ref('users/'+ accessCookie("name")).push({
+        chance: chance,
+        crush: person2.value,
+        name: person1.value
+    });
+
+}
+
+
+async function delay(delayInms) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(2);
+        }, delayInms);
+    });
+}
+
+
---
+++ cookies.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+function accessCookie(cookieName) {
+    var name = cookieName + "=";
+    var allCookieArray = document.cookie.split(';');
+    for (var i = 0; i < allCookieArray.length; i++) {
+        var temp = allCookieArray[i].trim();
+        if (temp.indexOf(name) == 0)
+            return temp.substring(name.length, temp.length);
+    }
+    return "";
+}
